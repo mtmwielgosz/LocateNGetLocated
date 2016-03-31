37b8f824bf98f1710f475bae858c7cbf93a7480f
b0GU94 <kamil.filip94@gmail.com>
2016-03-31 16:01:02 +0200

    #12
    Pokazywanie lokalizacji na mapie

--- app/src/main/java/com/locateandgetlocated/locategetlocated/MapsActivity.java
+++ app/src/main/java/com/locateandgetlocated/locategetlocated/MapsActivity.java
@@ -1,18 +1,28 @@
 package com.locateandgetlocated.locategetlocated;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private Double latitude=0.;
+    private Double longitude=0.;
+    private String date="Brak danych";
+    private String device_name="Brak danych";
 
     private GoogleMap mMap;
+    private CameraPosition cameraPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +35,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        /*Bundle pos = getIntent().getExtras();
+        if(!pos.isEmpty()){
+            latitude = Double.parseDouble(pos.getString("szerokosc"));
+            longitude = Double.parseDouble(pos.getString("dlugosc"));
+            date = pos.getString("data");
+            device_name = pos.getString("nazwa");}*/
+
+        LatLng place = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(place).title(device_name).snippet(date));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+
+        cameraPosition = new CameraPosition.Builder()
+                .target(place)      // Sets the center of the map to Mountain View
+                .zoom(13)                   // Sets the zoom
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+       /* if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+        mMap.setMyLocationEnabled(true);*/
+    }
+
+    public void setOnMixed(View view)
+    {
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+    }
+
+    public void setOnSatelite(View view)
+    {
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+    }
+
+    public void setOnTerrain(View view)
+    {
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+
+    }
+
+    public void setOnLocation(View view)
+    {
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 }
