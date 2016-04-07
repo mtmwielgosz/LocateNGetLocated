26a9c83b7f8217ce37325c46fa7af039987ccf91
b0GU94 <kamil.filip94@gmail.com>
2016-04-07 21:50:02 +0200

    #27, #28
    Przełączanie między różnymi widokami mapy.
    Przycisk pokazujący własną lokalizację.

--- app/src/main/java/com/locateandgetlocated/locategetlocated/MapsActivity.java
+++ app/src/main/java/com/locateandgetlocated/locategetlocated/MapsActivity.java
@@ -1,11 +1,15 @@
 package com.locateandgetlocated.locategetlocated;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,11 +25,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private String date="Brak danych";
     private String device_name="Brak danych";
     private String hour="Brak danych";
-
-    private GoogleMap mMap;
+    private Button button_Normal;
+    private Button button_Hybrid;
+    private Button button_Terrain;
+    private Button button_DeviceLocation;
     private CameraPosition cameraPosition;
+    private GoogleMap mMap;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
@@ -33,6 +39,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        button_Normal = (Button) findViewById(R.id.buttonNormalny);
+        button_Hybrid = (Button) findViewById(R.id.buttonSatelita);
+        button_Terrain = (Button) findViewById(R.id.buttonTeren);
+        button_DeviceLocation = (Button) findViewById(R.id.buttonLokalizacjaUrzadzenia);
+
+        button_DeviceLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setOnLocation(v);
+            }
+        });
+        button_Normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setOnSatelite(v);
+            }
+        });
+        button_Terrain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setOnTerrain(v);
+            }
+        });
+        button_Hybrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setOnMixed(v);
+            }
+        });
     }
 
 
@@ -51,29 +87,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng place = new LatLng(latitude, longitude);
         mMap.addMarker(new MarkerOptions().position(place).title(device_name).snippet(hour + " " + date));
         mMap.getUiSettings().setZoomControlsEnabled(true);
-
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.setMyLocationEnabled(true);
 
         cameraPosition = new CameraPosition.Builder()
                 .target(place)      // Sets the center of the map to Mountain View
                 .zoom(13)                   // Sets the zoom
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-       /* if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            return;
-        }
-        mMap.setMyLocationEnabled(true);*/
     }
 
     public void setOnMixed(View view)
     {
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
+
     }
 
     public void setOnSatelite(View view)
     {
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
 
     }
 
@@ -85,7 +119,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void setOnLocation(View view)
     {
+        mMap.setMyLocationEnabled(true);
+        Location myLocation = mMap.getMyLocation();
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
 
     }
 }
