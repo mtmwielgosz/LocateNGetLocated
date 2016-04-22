682fe9c3d22415204fe1d778632fa880a8d95998
mtmwielgosz <mtmwielgosz@gmail.com>
2016-04-22 10:04:39 +0200

    #13
    Historia lokalizacji + przesyłanie bundle.
    Symulacja

--- app/src/main/java/activities/DeviceHistoryActivity.java
+++ app/src/main/java/activities/DeviceHistoryActivity.java
@@ -20,18 +20,56 @@ public class DeviceHistoryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Samsung");
-        TextView temp = (TextView) findViewById(R.id.textView);
+
+
+
         final Bundle data = new Bundle();
 
-        data.putDouble("dlugosc",17.052048 );
-        data.putDouble("szerokosc", 51.101741 );
         data.putString("nazwa", "Samsung");
-        data.putString("data", "15.03.2016");
-        data.putString("godzina", "21:25");
-        temp.setOnClickListener(new View.OnClickListener() {
+        data.putDoubleArray("szerokosc", new double[]{51.101741, 53.101741, 52.101741, 54.101741});
+        data.putDoubleArray("dlugosc", new double[] {17.052048, 17.042048, 17.052048, 16.052048});
+        data.putStringArray("data", new String[]{"12-01-2016", "05-03-2016", "06-03-2016", "12-04-2016"});
+        data.putStringArray("godzina", new String[]{"21:25", "22:13", "13:12", "04:11"});
+
+        TextView temp1 = (TextView) findViewById(R.id.textView);
+        temp1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                data.putInt("indeks", 0);
+                intent.putExtras(data);
+                startActivity(intent);
+            }
+        });
+
+        TextView temp2 = (TextView) findViewById(R.id.textView1);
+        temp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                data.putInt("indeks", 1);
+                intent.putExtras(data);
+                startActivity(intent);
+            }
+        });
+
+        TextView temp3 = (TextView) findViewById(R.id.textView2);
+        temp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                data.putInt("indeks", 2);
+                intent.putExtras(data);
+                startActivity(intent);
+            }
+        });
+
+        TextView temp4 = (TextView) findViewById(R.id.textView3);
+        temp4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                data.putInt("indeks", 3);
                 intent.putExtras(data);
                 startActivity(intent);
             }
