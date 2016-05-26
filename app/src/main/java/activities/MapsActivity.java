2a84fb5e904bced803a0c8dc932f9690f5905d44
b0GU94 <kamil.filip94@gmail.com>
2016-05-26 18:26:55 +0200

    #79 - wersja testowa
    Dodano możliwość zaznaczania przedziału czasowego po długim naciśnięciu
    dwóch pozycji na osi czasu.

--- app/src/main/java/activities/MapsActivity.java
+++ app/src/main/java/activities/MapsActivity.java
@@ -1,14 +1,17 @@
 package activities;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,6 +20,7 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,6 +28,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.locateandgetlocated.locategetlocated.R;
 
@@ -31,7 +37,6 @@ import database.DBHandler;
 import database.Request;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,6 +59,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private TextView adressMediumTB;
     private CameraPosition cameraPosition;
     static private GoogleMap mMap;
+    private int[] timePeriodIndex = new int[2];
 
     Place[] locations;
     private List<Place> currentPlaces  = new ArrayList<Place>(); ;
@@ -73,6 +79,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        initializeInterface();
+        //pobiera lokacje z bazy danych
+        setPlaces();
+
+        //wstawia przykładowe dane z lokacjami
+        //setPlacesTMP();
+        createTimeline();
+
+    }
+
+
+    public void initializeInterface(){
         deviceNumber = getIntent().getStringExtra("name");
         deviceName = getIntent().getStringExtra("deviceName");
         counter = (TextView) findViewById(R.id.counter);
@@ -83,18 +101,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapViewSpinner = (Spinner) findViewById(R.id.mapViewMode);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.mapViewsArray, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         mapViewSpinner.setAdapter(adapter);
         mapViewSpinner.setOnItemSelectedListener(new SpinnerActivity());
-        setPlacesTMP();
-        createTimeline();
-
     }
-
-
-
     public void setAdress(LatLng l){
         List<Address> locations = null;
         String adress = "";
@@ -139,12 +149,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Location lok = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         if(lok!=null) {
             l = new LatLng(lok.getLatitude(), lok.getLongitude());
-           // mMap.addMarker(new MarkerOptions().position(l).title("Tu jesteś!").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin1)));
             mMap.addMarker(new MarkerOptions().position(l).title("Tu jesteś!").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         }
             return l;
     }
-
 
 
     public void setSelectedPosition(LatLng p, String d, String h){
@@ -153,43 +161,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.clear();
         mMap.addMarker(new MarkerOptions().position(p).title(deviceNumber).snippet(h + " " + d));
 
-        if(lastPosition==null){
-            lastPosition=new Place(new LatLng(p.latitude,p.longitude),d,h);
-        }
-        else {
-            mMap.addMarker(new MarkerOptions().position(lastPosition.getCoordinates()).title(deviceNumber).snippet(lastPosition.getHour() + " " + lastPosition.getDate()));
-        }
-
+       setLastPosition();
         double latitudeAVG = (p.latitude + lastPosition.getCoordinates().latitude)/2;
         double longitudeAVG =   (p.longitude + lastPosition.getCoordinates().longitude)/2;
 
-        int distance =  ((int) calculateDistance(p,locations[locations.length-1].getCoordinates()))/1000;
+        int distance =   (int) calculateDistance(p,locations[locations.length-1].getCoordinates())/1000;
 
 
         cameraPosition = new CameraPosition.Builder()
                 .target(new LatLng(latitudeAVG, longitudeAVG))// Sets the center of the map to Mountain View
-                .zoom(distance/200)                   // Sets the zoom
+                .zoom(8)                   // Sets the zoom
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-       setAdress(p);
+        setAdress(p);
 
         distance = 0;
 
         if( getCurrentPosition()!=null){
             distance = (int) calculateDistance(p, getCurrentPosition());
         }
-       // float distance = calculateDistance(p, new LatLng(51.0, 17.0));
         if(distance<1000) {
             distanceTB.setText( distance + " m");
         }else{
-            distanceTB.setText( distance/1000 + " km");
+            distanceTB.setText(distance / 1000 + " km");
         }
 
     }
-
-
-
-
     public void setOnSatelite(View view)
     {
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -204,6 +201,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void setPlacesTMP(){
+
+        locations = new Place[10];
+        locations[0] = new Place(new LatLng(51.197282, 15.397279 ),"12-03-2016","11:01");
+        locations[1] = new Place(new LatLng(51.297282, 15.357279 ),"12-03-2016","14:21");
+        locations[2] = new Place(new LatLng( 51.147282, 15.327279),"13-03-2016","11:13");
+        locations[3] = new Place(new LatLng( 51.157282, 15.497279),"15-03-2016","16:07");
+        locations[4] = new Place(new LatLng(51.177282, 15.197279 ),"15-03-2016","21:29");
+        locations[5] = new Place(new LatLng(51.497282, 15.394279 ),"15-03-2016","22:04");
+        locations[6] = new Place(new LatLng(51.187282, 15.399279 ),"16-03-2016","01:47");
+        locations[7] = new Place(new LatLng(51.397282, 15.395279 ),"17-03-2016","07:55");
+        locations[8] = new Place(new LatLng(51.187282, 14.997279 ),"17-03-2016","14:45");
+        locations[9] = new Place(new LatLng(51.297282, 15.387232 ),"17-03-2016","19:58");
+
+    }
+
+    public void setPlaces(){
         dbHandler = new DBHandler(this, null, null, 1);
         //dbHandler.addRequest(new Request(10,53.1,17.3,null,null,null,"k"));
         int reqestId = getIntent().getIntExtra("id", 1);
@@ -218,7 +231,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (requests[i].receiver.equals(deviceNumber) && requests[i].localizationDate!=null) {
                 Log.d("WCHODZ", requests[i].receiver + "==" + deviceNumber);
 
-//                locations[i]=new Place(new LatLng(51.10828596112606 + rand.nextDouble(),17.05601692199707 + rand.nextDouble()),(i+10)+".05.2016","10:"+(i+10));
                 places.add(new Place(new LatLng(requests[i].latitude, requests[i].longitude),new java.sql.Date(requests[i].localizationDate.getTime())+"", requests[i].localizationDate.getHours()+ ":" +requests[i].localizationDate.getMinutes() ));
 
                 if(reqestId==requests[i].id){
@@ -234,6 +246,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    protected void setLastPosition(){
+        if(lastPosition==null){
+
+            lastPosition=new Place(new LatLng(locations[locations.length -1].getCoordinates().latitude,locations[locations.length -1].getCoordinates().longitude),locations[locations.length -1].getDate(),locations[locations.length -1].getHour());}
+        else {
+          //  mMap.addMarker(new MarkerOptions().position(lastPosition.getCoordinates()).title(deviceNumber).snippet(lastPosition.getHour() + " " + lastPosition.getDate()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));}
+
+
+    }}
+    protected void setMarkers() {
+        mMap.clear();
+
+
+
+        LatLngBounds.Builder b = new LatLngBounds.Builder();
+        double distance = calculateDistance(locations[timePeriodIndex[0]].getCoordinates(), locations[1].getCoordinates());
+        for (int i = timePeriodIndex[0]; i < timePeriodIndex[1] + 1; i++) {
+            b.include(locations[i].getCoordinates());
+            mMap.addMarker(new MarkerOptions().position(locations[i].getCoordinates()).title(locations[i].getHour()).snippet(locations[i].getDate()).icon(BitmapDescriptorFactory.fromResource(R.drawable.dot)));
+        }
+        LatLngBounds bounds = b.build();
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 4);
+        mMap.animateCamera(cu);
+    }
+
+   protected void markTimePeriodOnTimeline(int start, int stop, boolean makeClear){
+       if(!makeClear) {
+           for (int i = start; i < stop; i++) {
+               dateButtons[i].setBackgroundColor(Color.rgb(128, 128, 128));
+           }
+       }else{for (int i = start; i < stop; i++) {
+           dateButtons[i].setBackgroundColor(Color.rgb(255, 204, 102));
+       }}
+   }
     protected void createTimeline(){
 
         timelineSV = (HorizontalScrollView ) findViewById(R.id.timelineSV);
@@ -244,6 +290,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             final int z =i;
             dateButtons[i] = new Button(this);
             dateButtons[i].setText(locations[i].getHour() + "\n" + locations[i].getDate());
+            dateButtons[i].setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
             dateButtons[i].setTextColor(Color.rgb(255, 255, 255));
             dateButtons[i].setBackgroundColor(Color.rgb(128, 128, 128));
             dateButtons[i].setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
@@ -251,13 +298,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             dateButtons[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
                     dateButtons[selectedIndex].setBackgroundColor(Color.rgb(128, 128, 128));
+                    //wyczysc
+                    markTimePeriodOnTimeline(0,dateButtons.length,false);
                     selectedIndex = z;
                     dateButtons[z].setBackgroundColor(Color.rgb(0, 0, 0));
                     setSelectedPosition(locations[z].getCoordinates(), locations[z].getDate(), locations[z].getHour());
                 }
 
             });
+            dateButtons[i].setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    //wyczysc wszystko
+                    markTimePeriodOnTimeline(0,dateButtons.length,false);
+
+                    if(timePeriodIndex[0]==-1 && timePeriodIndex[1]==-1){
+                        timePeriodIndex[0]=z;}
+                    else if(timePeriodIndex[0]!= -1 && timePeriodIndex[1]== -1) {
+                        if (timePeriodIndex[0] > z) {
+                            timePeriodIndex[1] = timePeriodIndex[0];
+                            timePeriodIndex[0] = z;
+
+                            //ustaw
+                            markTimePeriodOnTimeline(timePeriodIndex[0],timePeriodIndex[1] + 1,true);
+
+                        } else {
+                            timePeriodIndex[1] = z;
+                            //ustaw
+                            markTimePeriodOnTimeline(timePeriodIndex[0],timePeriodIndex[1] + 1,true);
+
+                        }
+                        setMarkers();
+
+                    }
+                    else if (timePeriodIndex[0] != -1 && timePeriodIndex[1] != -1) {
+                       //mMap.clear();
+                        //wyczysc
+                        markTimePeriodOnTimeline(timePeriodIndex[0],timePeriodIndex[1] + 1,false);
+
+                        timePeriodIndex[0]=z;
+                        timePeriodIndex[1]=-1;
+                        dateButtons[z].setBackgroundColor(Color.rgb(255, 204, 102));
+                    }
+                    return true;
+                    }
+
+
+                }
+            );
 
         }
         timelineSV.post(new Runnable() {
