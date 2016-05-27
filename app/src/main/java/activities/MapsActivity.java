d503591cf12928fcf99b27309fb2448eb350ac25
b0GU94 <kamil.filip94@gmail.com>
2016-05-27 20:30:30 +0200

    #84
    - usunięcie błędu związanego z translacją adresu (brak połączenia wifi)
    - translacja adresu działa teraz jako osobny wątek

--- app/src/main/java/activities/MapsActivity.java
+++ app/src/main/java/activities/MapsActivity.java
@@ -1,13 +1,14 @@
 package activities;
 
-import android.annotation.TargetApi;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.Build;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,7 @@ import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -33,10 +35,13 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.locateandgetlocated.locategetlocated.R;
 
+import org.w3c.dom.Text;
+
 import database.DBHandler;
 import database.Request;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,18 +56,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationManager locationManager;
     private Place lastPosition;
     private int current_indeks=22;
-    private Geocoder geoCoder = null;
+    private Geocoder geoCoder=null;
     private TextView dataTB;
     private TextView distanceTB;
     private TextView counter;
-    private TextView adressTB;
-    private TextView adressMediumTB;
+    static private TextView adressTB;
+    static private TextView adressMediumTB;
     private CameraPosition cameraPosition;
     static private GoogleMap mMap;
     private int[] timePeriodIndex = new int[2];
 
     Place[] locations;
-    private List<Place> currentPlaces  = new ArrayList<Place>(); ;
     private HorizontalScrollView timelineSV;
     private LinearLayout timelineLayout;
     private Button[] dateButtons;
@@ -86,12 +90,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //wstawia przykładowe dane z lokacjami
         //setPlacesTMP();
         createTimeline();
-
     }
 
 
     public void initializeInterface(){
+
+        geoCoder = new Geocoder(this);
         deviceNumber = getIntent().getStringExtra("deviceNumber");
+
         deviceName = getIntent().getStringExtra("deviceName");
         counter = (TextView) findViewById(R.id.counter);
         dataTB = (TextView) findViewById(R.id.dataTB);
@@ -104,43 +110,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mapViewSpinner.setAdapter(adapter);
         mapViewSpinner.setOnItemSelectedListener(new SpinnerActivity());
+
     }
-    public void setAdress(LatLng l){
-        List<Address> locations = null;
-        String adress = "";
-        geoCoder = new Geocoder(this);
-        try {
-            locations= geoCoder.getFromLocation(l.latitude,l.longitude,4);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void setAdress(LatLng d){
+
+
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    final LatLng l =d;
+        Boolean present=geoCoder.isPresent();
+        if (mWifi.isConnected()) {
+            if(present!=null && present){
+            adressMediumTB.post(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+                        String adress = "";
+                        List<Address> locations = geoCoder.getFromLocation(l.latitude, l.longitude, 1);
+                        if (locations.size() > 0) {
+                            for (int i = 1; i < locations.get(0).getMaxAddressLineIndex() + 1; i++) {
+                                adress = adress + locations.get(0).getAddressLine(i) + ", ";
+                            }
+                            setAdressTextbox(adress,locations.get(0).getAddressLine(0) + "");
+                        }
+                    }
+                    catch(IOException e) {
+                        e.printStackTrace();}
+                }});
+         }}else{
+            adressTB.setText("Translacja adresu niedostępna");
+            adressMediumTB.setText("Wymagane połączenie z siecią WiFi");}
         }
 
-        if(locations != null && locations.size() > 0) {
-            for (int i = 1; i < locations.get(0).getMaxAddressLineIndex() + 1; i++) {
 
-                adress = adress + locations.get(0).getAddressLine(i) + ", ";
-            }
-            adressTB.setText(adress);
-            adressMediumTB.setText(locations.get(0).getAddressLine(0) + "");
-        }
-    }
-
-
-    @Override
+        @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.setMyLocationEnabled(true);
-        setSelectedPosition(locations[locations.length - 1].getCoordinates(), locations[locations.length - 1].getDate(), locations[locations.length - 1].getHour());
-    }
+        if(locations!=null && locations.length>0){
+            setSelectedPosition(locations[selectedIndex].getCoordinates(), locations[selectedIndex].getDate(), locations[selectedIndex].getHour());
+        setAdress(locations[selectedIndex].getCoordinates());
+        }}
 
     public float calculateDistance(LatLng loc1, LatLng loc2){
         float[] results = new float[1];
         Location.distanceBetween(loc1.latitude, loc1.longitude, loc2.latitude, loc2.longitude, results);
         return results[0];
     }
-
 
 
     public LatLng getCurrentPosition(){
@@ -159,13 +178,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         dataTB.setText(deviceName);
         counter.setText((selectedIndex + 1) + "/" + locations.length);
         mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(p).title(deviceNumber).snippet(h + " " + d));
+        mMap.addMarker(new MarkerOptions().position(p).title(deviceName).snippet(h + " " + d));
 
         setLastPosition();
         double latitudeAVG = (p.latitude + lastPosition.getCoordinates().latitude)/2;
         double longitudeAVG =   (p.longitude + lastPosition.getCoordinates().longitude)/2;
 
-        int distance =   (int) calculateDistance(p,locations[locations.length-1].getCoordinates())/1000;
+        //int distance =   (int) calculateDistance(p,locations[locations.length-1].getCoordinates())/1000;
 
 
         cameraPosition = new CameraPosition.Builder()
@@ -173,12 +192,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .zoom(8)                   // Sets the zoom
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        setAdress(p);
 
-        distance = 0;
-
-        if( getCurrentPosition()!=null){
-            distance = (int) calculateDistance(p, getCurrentPosition());
+        int distance = 0;
+        LatLng currentPosition = getCurrentPosition();
+        if( currentPosition!=null){
+            distance = (int) calculateDistance(p, currentPosition);
         }
         if(distance<1000) {
             distanceTB.setText( distance + " m");
@@ -199,7 +217,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
-
     public void setPlacesTMP(){
 
         locations = new Place[10];
@@ -213,14 +230,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locations[7] = new Place(new LatLng(51.397282, 15.395279 ),"17-03-2016","07:55");
         locations[8] = new Place(new LatLng(51.187282, 14.997279 ),"17-03-2016","14:45");
         locations[9] = new Place(new LatLng(51.297282, 15.387232 ),"17-03-2016","19:58");
-
+        selectedIndex=9;
     }
-
     public void setPlaces(){
         dbHandler = new DBHandler(this, null, null, 1);
         //dbHandler.addRequest(new Request(10,53.1,17.3,null,null,null,"k"));
         int reqestId = getIntent().getIntExtra("id", 1);
-
 
         Request[] requests = dbHandler.getRequestsArray();
 
@@ -228,7 +243,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         for(int i = 0; i < requests.length; i++) {
             Log.d("NIE_WCHODZI", requests[i].receiver + "==" + deviceNumber);
-            if (requests[i].receiver.equals(deviceNumber) && requests[i].localizationDate!=null) {
+            if (requests[i].receiver.equals(deviceNumber) && !(new java.sql.Date(requests[i].localizationDate.getTime())+"").equals("1970-01-01")) {
                 Log.d("WCHODZ", requests[i].receiver + "==" + deviceNumber);
 
                 places.add(new Place(new LatLng(requests[i].latitude, requests[i].longitude),new java.sql.Date(requests[i].localizationDate.getTime())+"", requests[i].localizationDate.getHours()+ ":" +requests[i].localizationDate.getMinutes() ));
@@ -251,14 +266,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             lastPosition=new Place(new LatLng(locations[locations.length -1].getCoordinates().latitude,locations[locations.length -1].getCoordinates().longitude),locations[locations.length -1].getDate(),locations[locations.length -1].getHour());}
         else {
-            //  mMap.addMarker(new MarkerOptions().position(lastPosition.getCoordinates()).title(deviceNumber).snippet(lastPosition.getHour() + " " + lastPosition.getDate()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));}
+          //  mMap.addMarker(new MarkerOptions().position(lastPosition.getCoordinates()).title(deviceNumber).snippet(lastPosition.getHour() + " " + lastPosition.getDate()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));}
+    }}
 
-
-        }}
     protected void setMarkers() {
         mMap.clear();
-
-
+        if(!locations[timePeriodIndex[0]].getDate().equals(locations[timePeriodIndex[1]].getDate())) {
+            adressMediumTB.setText("od  "  + locations[timePeriodIndex[0]].getDate() + "  do  " + locations[timePeriodIndex[1]].getDate());
+        }else{            adressMediumTB.setText( locations[timePeriodIndex[0]].getDate());
+        }
+            adressTB.setText("Wybrane lokalizacje");
 
         LatLngBounds.Builder b = new LatLngBounds.Builder();
         double distance = calculateDistance(locations[timePeriodIndex[0]].getCoordinates(), locations[1].getCoordinates());
@@ -283,8 +300,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void createTimeline(){
 
         timelineSV = (HorizontalScrollView ) findViewById(R.id.timelineSV);
+        timelineSV.setBackgroundColor(Color.rgb(128, 128, 128));
         dateButtons=new Button[locations.length];
         timelineLayout = (LinearLayout) findViewById(R.id.timelineLayout);
+        timelineLayout.setBackgroundColor(Color.rgb(128, 128, 128));
 
         for(int i=0; i<locations.length; i++){
             final int z =i;
@@ -305,6 +324,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     selectedIndex = z;
                     dateButtons[z].setBackgroundColor(Color.rgb(0, 0, 0));
                     setSelectedPosition(locations[z].getCoordinates(), locations[z].getDate(), locations[z].getHour());
+                    setAdress(locations[z].getCoordinates());
+
                 }
 
             });
@@ -355,8 +376,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 timelineSV.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
             }
         });
-        dateButtons[selectedIndex].setBackgroundColor(Color.rgb(0, 0, 0));        //timelineSV.scrollTo(0, timelineSV.getPaddingEnd());
+        if(locations.length>0) {
+            dateButtons[selectedIndex].setBackgroundColor(Color.rgb(0, 0, 0));        //timelineSV.scrollTo(0, timelineSV.getPaddingEnd());
+        }}
+
+
+    public void setAdressTextbox(String s1, String s2){
+        adressTB.setText(s1);
+        adressMediumTB.setText(s2);
     }
+
+    }
+
+ /*class GeoCoding extends Thread  {
+
+    List<Address> locations = null;
+    String adress = "";
+    Geocoder geocoder;
+    String text="-";
+    String textMedium="-";
+    LatLng l;
+
+    public GeoCoding(Geocoder g, LatLng l) {
+       geocoder=g;
+        this.l=l;
+    }
+=======
 }
+>>>>>>> origin/master
+
+    @Override
+    public void run() {
+
+          //  try {
+                try
+
+                {
+                    locations = geocoder.getFromLocation(l.latitude, l.longitude, 1);
+                    if (locations.size() > 0) {
+                        for (int i = 1; i < locations.get(0).getMaxAddressLineIndex() + 1; i++) {
+
+                            adress = adress + locations.get(0).getAddressLine(i) + ", ";
+                        }
+                       text=adress;
+                       textMedium=locations.get(0).getAddressLine(0) + "";
+
+
+                    }
+                }
+
+                catch(
+                        IOException e
+                        )
+
+                {
+                    e.printStackTrace();
+                }
+
+            } //catch (InterruptedException e) {
+            //    e.printStackTrace();
+         //   }
+        }
+
+*/
+
+
+
 
 
