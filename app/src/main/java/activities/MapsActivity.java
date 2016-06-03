b1ab9c24528b2b6802eaef6070903d8bc3b9c514
mtmwielgosz <mtmwielgosz@gmail.com>
2016-06-03 12:49:12 +0200

    MAGIA

--- app/src/main/java/activities/MapsActivity.java
+++ app/src/main/java/activities/MapsActivity.java
@@ -2,6 +2,7 @@ package activities;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
@@ -444,7 +445,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         adressMediumTB.setText(s2);
     }
 
+    @Override
+    public void onBackPressed()
+    {
+        if(getIntent().getBooleanExtra("fromNotify", false))
+        {
+            startActivity(new Intent(getApplicationContext(), LocalizationActivity.class));
+        }
+        else
+            super.onBackPressed();
     }
+
+}
 
 
 
